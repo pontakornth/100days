5b93ed481f93d200c4a89db95165e15820766081
pontakornth <pontakorn61@gmail.com>
2021-03-22 14:20:02 +0700

    feat: Make content base on markdown

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="text-5xl text-center font-bold">100 Days of Code</h1>
-    </div>
-  </div>
+  <main class="container">
+    <h1 class="text-5xl text-center font-bold mb-8">100 Days of Code</h1>
+    <project-block
+      v-for="project in projects"
+      :key="project"
+      :link="project.link"
+      :day="project.day"
+      :document="project"
+      :title="project.title"
+    />
+  </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import ProjectBlock from '../components/ProjectBlock.vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: {
+    ProjectBlock,
+  },
+  asyncData: async ({ $content }) => {
+    const projects = await $content('/').fetch()
+    return { projects }
+  },
+})
 </script>
 
 <style>

282a1aa043dbbd5d3dc98a330bc1b7d8cb5c1ea0
pontakornth <pontakorn61@gmail.com>
2021-03-22 13:49:57 +0700

    feat: Add project component

---
+++ components/ProjectBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ tiitle }}</h1>
+    <a :href="link">{{ link }}</a>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>

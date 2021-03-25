23037233bd5e0750f7acd9916488a7c38ef26b89
pontakornth <pontakorn61@gmail.com>
2021-03-25 22:50:35 +0700

    feat: Add day article

---
+++ pages/posts/_day.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="container mx-auto p-8">
+    <h1 class="text-5xl text-center font-bold mb-8 mx-auto">
+      {{ project.title }}
+    </h1>
+    <div class="bg-white border shadow-md rounded p-4">
+      <nuxt-content :document="project" />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { IContentDocument } from '@nuxt/content/types/content'
+import Vue from 'vue'
+export default Vue.extend({
+  async asyncData({ route, $content }) {
+    const day = Number.parseInt(route.params.day)
+    const project = ((await $content('/')
+      .where({ day: { $eq: day } })
+      .fetch()) as unknown) as IContentDocument[]
+    return { project: project[0] }
+  },
+})
+</script>

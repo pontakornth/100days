a68f09a9f0ffe2e1c5698a99633e293c6d10ddcf
pontakornth <pontakorn61@gmail.com>
2021-04-11 22:27:19 +0700

    feat: Add redirect

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <page-template :projects="projects" />
+  <page-template :projects="projects" :page="1" />
 </template>
 
 <script lang="ts">
@@ -9,6 +9,9 @@ import PageTemplate from '~/components/PageTemplate.vue'
 export default Vue.extend({
   components: {
     PageTemplate,
+  },
+  middleware({ redirect }): void {
+    return redirect(301, '/page/1')
   },
   asyncData: async ({ $content }) => {
     const projects = await $content('/').sortBy('day', 'desc').fetch()

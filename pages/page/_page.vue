0708c0c282953629562cd7126411aed00be94207
pontakornth <pontakorn61@gmail.com>
2021-04-11 22:10:03 +0700

    fix: wrong filename

---
+++ pages/page/_page.vue
@@ -0,0 +1,49 @@
+<template>
+  <page-template :projects="paginatedProjects" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import PageTemplate from '~/components/PageTemplate.vue'
+export default Vue.extend({
+  components: { PageTemplate },
+  asyncData: async ({ $content, params, error }) => {
+    const currentPage = parseInt(params.page)
+    const perPage = 5
+    const allProjects = await $content('/').fetch()
+    const totalProjects = allProjects.length
+
+    const lastPage = Math.ceil(totalProjects / perPage)
+    const lastPageCount = totalProjects % perPage
+    const skipProject = () => {
+      switch (currentPage) {
+        case 1:
+          return 0
+        case lastPage:
+          return totalProjects - lastPageCount
+        default:
+          return (currentPage - 1) * perPage
+      }
+    }
+    const paginatedProjects = await $content('/')
+      .sortBy('day', 'desc')
+      .skip(skipProject())
+      .limit(perPage)
+      .fetch()
+
+    if (currentPage === 0 || !paginatedProjects.length) {
+      return error({ statusCode: 404, message: 'No projects found!' })
+    }
+
+    return { paginatedProjects }
+  },
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  @apply p-4;
+}
+</style>

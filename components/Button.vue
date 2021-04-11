aa2050ba9f3144c3a5013e7a1ed09e7638c9e4dd
pontakornth <pontakorn61@gmail.com>
2021-04-11 22:27:33 +0700

    feat: Add button for pagination

---
+++ components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <nuxt-link
+    :to="to"
+    class="p-4 rounded bg-white hover:bg-gray-200 font-bold shadow-md"
+  >
+    <slot></slot>
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>

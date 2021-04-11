869a15676f66e504e8254b089089dd6403ae663a
pontakornth <pontakorn61@gmail.com>
2021-04-11 21:27:03 +0700

    refactor: Make template seperate

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,14 @@
 <template>
-  <main class="container">
-    <h1 class="text-5xl text-center font-bold mb-4">100 Days of Code</h1>
-    <span class="block text-center text-2xl text-gray-500 mb-8"
-      >Pontakorn Paesaeng (<a
-        href="https://github.com/pontakornth"
-        target="__blank"
-        class="underline text-gray-600"
-        >@pontakornth</a
-      >)</span
-    >
-    <project-block
-      v-for="project in projects"
-      :key="project.day"
-      :link="project.link"
-      :day="project.day"
-      :document="project"
-      :title="project.title"
-    />
-  </main>
+  <page-template :projects="projects" />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import ProjectBlock from '../components/ProjectBlock.vue'
+import PageTemplate from '~/components/PageTemplate.vue'
 
 export default Vue.extend({
   components: {
-    ProjectBlock,
+    PageTemplate,
   },
   asyncData: async ({ $content }) => {
     const projects = await $content('/').sortBy('day', 'desc').fetch()
